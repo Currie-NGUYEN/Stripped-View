242a9f458fcee73abbfd984decd3e1fad848153d
currie-NGUYEN <currie@enclave.vn>
2020-03-31 10:30:21 +0700

    Initial Commit

---
+++ Stripped View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stripped View
+//
+//  Created by Currie on 3/31/20.
+//  Copyright © 2020 Currie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
